cf65416b3cdda948f482c4b575afe70902812532
Cassfsd <perezcasandra@outlook.com>
2023-03-11 19:49:19 -0300

    first commit

---
+++ practicaClase39.js
@@ -0,0 +1,31 @@
+var miPersona = [
+    {
+        nombre: "Casandra",
+        apellido: "Pérez",
+        edad: 31,
+        trabaja: true,
+        hobbies: 
+            ["idiomas", "leer","escribir" ]
+        
+    },
+    {
+        vehiculo: true,
+        tipo: "moto",
+        color: "azul",
+        registro: "vencido"
+    },
+
+    {
+
+        vivienda: "alquiler",
+        direccion: "Balvanera",
+        habitaciones: 1,
+        ducha: "compartida",
+        metrosCuadrados: 16,
+        wifi: true,
+        tvPorCable: false,
+        calefaccion: false
+    }
+];
+
+console.log(miPersona);
